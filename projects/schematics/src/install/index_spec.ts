460f8406b19985472122bb2c8ec748e548f2e4c0
sis0k0 <stanimira.vlaeva@gmail.com>
2018-09-24 14:07:05 +0300

    test(unit): setup test project

--- projects/schematics/src/install/index_spec.ts
+++ projects/schematics/src/install/index_spec.ts
@@ -1,16 +1,23 @@
 import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 import { PackageJson } from '../utils/package-json';
 import { PackageInfo } from '../utils/package-info';
+import { createTestApp } from '../utils/test-utils';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
 describe('Install schematic', () => {
+  let runner: SchematicTestRunner;
+  let tree: UnitTestTree;
+
+  beforeEach(() => {
+    const appTree = createTestApp({ name: 'myProject' });
+    runner = new SchematicTestRunner('schematics', collectionPath);
+    tree = runner.runSchematic('ng-add', {}, appTree);
+  });
+
   it('adds dependency to the library in package.json', () => {
-    const runner =
-      new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('ng-add', {}, Tree.empty());
 
     const packageJsonPath = '/package.json';
     expect(tree.files).toContain(packageJsonPath);
