708fd09ea754e3bbc5a1f9a24332aba8680703e7
sis0k0 <stanimira.vlaeva@gmail.com>
2018-09-24 14:01:04 +0300

    test(unit): ensure that a dependency to the library is added

--- projects/schematics/src/install/index_spec.ts
+++ projects/schematics/src/install/index_spec.ts
@@ -1,16 +1,21 @@
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
-
+import { PackageJson } from '../utils/package-json';
+import { PackageInfo } from '../utils/package-info';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
+describe('Install schematic', () => {
+  it('adds dependency to the library in package.json', () => {
+    const runner =
+      new SchematicTestRunner('schematics', collectionPath);
+    const tree = runner.runSchematic('ng-add', {}, Tree.empty());
 
-describe('schematics', () => {
-  it('works', () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('schematics', {}, Tree.empty());
+    const packageJsonPath = '/package.json';
+    expect(tree.files).toContain(packageJsonPath);
 
-    expect(tree.files).toEqual([]);
+    const packageJson: PackageJson = JSON.parse(tree.readContent(packageJsonPath));
+    expect(packageJson.dependencies[PackageInfo.name]).toEqual(PackageInfo.version);
   });
 });
